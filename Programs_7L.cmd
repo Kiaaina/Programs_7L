13680db8bef37ab4436130eed06f670ad1f9a4f0
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-27 17:04:10 -1000

    Update Programs_7L.cmd

--- Programs_7L.cmd
+++ Programs_7L.cmd
@@ -1,19 +1,20 @@
 cd /
 :top
 cls
-@cecho off
+@echo off
 title "Programs_7L"
 mode 10,20
+@set "cecho=.\cecho.exe"
 @set "worp=314571258164"
 @set "jester=191854321867"
 @set "napster=141815620918"
 @set "imaxi=194321687191"
-cecho {09}1=worp2{\n}{04}2=jester2{\n}{03}3=napster2{\n}4=imaxi{05}
-set /a m=choose;
-if (%m%=1) & goto worp;
-if (%m%=2) & goto jester;
-if (%m%=3) & goto napster;
-if (%m%=4) & goto imaxi;
+%cecho% {09}1=worp2{\n}{04}2=jester2{\n}{03}3=napster2{\n}4=imaxi{05}
+set /p m=choose;
+if (%m%=1) goto worp;
+if (%m%=2) goto jester;
+if (%m%=3) goto napster;
+if (%m%=4) goto imaxi;
 goto top
 
 :worp
@@ -30,6 +31,6 @@ copy "%imaxi% ./imaxi.iso"
 goto 7L
 
 :7L
-cecho {09}By_Kia7L{02}
+%cecho% {09}By_Kia7L{02}
 timeout /t 5
 goto top
